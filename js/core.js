9fd6898543176cb92a658dec704a1d5dc966333c
fly <franck.ly247@gmail.com>
2017-09-11 10:07:14 -0400

    wall + movement

--- js/core.js
+++ js/core.js
@@ -1,8 +1,13 @@
+const MAX_HEIGHT = 490;
+const MAX_WIDTH = 600;
+
 var container;
-var myGamePiece;
-var myObstacles = [];
-var myScore;
-var metaPressed = false;
+var player;
+var score;
+var ctrlPressed = false;
+var gameStarted = false;
+var gameOver = false;
+var permanentWall = [];
 
 var KEY = {
   Z: 90,
@@ -21,202 +26,129 @@ var KEY = {
   SHIFT: 16,
   ALT: 18,
   ESC: 27,
+  TAB: 9,
+  E: 69,
 };
 
-function component(width, height, color, x, y, type) {
-  this.type = type;
-  this.score = 0;
-  this.width = width;
-  this.height = height;
-  this.speedX = 0;
-  this.x = x;
-  this.y = y;
-  this.gravity = 0;
-  this.gravitySpeed = 0;
-  this.update = function() {
-    ctx = container.context;
-    if (this.type == "text") {
-      ctx.font = this.width + " " + this.height;
-      ctx.fillStyle = color;
-      ctx.fillText(this.text, this.x, this.y);
-    } else {
-      ctx.fillStyle = color;
-      ctx.fillRect(this.x, this.y, this.width, this.height);
-    }
-  }
-  this.newPos = function() {
-    this.gravitySpeed += this.gravity;
-    this.x += this.speedX;
-    this.y += this.gravitySpeed;
-    this.hitContainer();
-  }
-  this.hitContainer = function() {
-    var bottom = container.canvas.height - this.height;
-    var right = container.canvas.width - this.width;
-    if (this.y <= 0) {
-      this.y = 0;
-      this.gravitySpeed = 0;
-    } else if (this.y >= bottom) {
-      this.y = bottom;
-      this.gravitySpeed = 0;
-    }
-    if (this.x <= 0) {
-      this.x = 0;
-      this.speedX = 0;
-    } else if (this.x >= right) {
-      this.x = right;
-      this.speedX = 0;
-    }
-  }
-  this.crashWith = function(o) {
-    return !(((this.y + (this.height)) < o.y) ||
-      (this.y > (o.y + (o.height))) ||
-      ((this.x + (this.width)) < o.x) ||
-      (this.x > (o.x + (o.width))));
-  }
-}
+var KEY_UP = [KEY.UP, KEY.W, KEY.Z, KEY.SPACE];
+var KEY_DOWN = [KEY.DOWN, KEY.S];
+var KEY_LEFT = [KEY.LEFT, KEY.A, KEY.Q];
+var KEY_RIGHT = [KEY.RIGHT, KEY.D];
+var KEY_ACTION = [KEY.E];
 
 function updateGameArea() {
-  var x, height, gap, minHeight, maxHeight, minGap, maxGap;
-  for (i = 0; i < myObstacles.length; i += 1) {
-    if (myGamePiece.crashWith(myObstacles[i])) {
-      return;
-    }
+  if (player.isDead()) {
+    gameOver = true;
+    return;
   }
   container.clear();
   container.frameNo += 1;
-  if (container.frameNo == 1 || everyinterval(150)) {
-    x = container.canvas.width;
-    minHeight = 20;
-    maxHeight = 200;
-    height = Math.floor(Math.random() * (maxHeight - minHeight + 1) + minHeight);
-    minGap = 50;
-    maxGap = 200;
-    gap = Math.floor(Math.random() * (maxGap - minGap + 1) + minGap);
-    myObstacles.push(new component(10, height, "#CC0", x, 0));
-    myObstacles.push(new component(10, x - height - gap, "#CC0", x, height + gap));
-  }
-  for (i = 0; i < myObstacles.length; i += 1) {
-    myObstacles[i].x += -1;
-    myObstacles[i].update();
-  }
-  myScore.text = "SCORE: " + container.frameNo;
-  myScore.update();
-  myGamePiece.newPos();
-  myGamePiece.update();
-}
+  
+  score.text = `SCORE: ${container.frameNo}`;
+  score.update();
 
-function everyinterval(n) {
-  return (container.frameNo / n) % 1 == 0;
+  permanentWall.forEach(wall => { wall.update() });
+  player.newPos(permanentWall);
+  player.update();
 }
 
 function startGame() {
-  container.clear();
-  myGamePiece = new component(30, 30, "#C00", 10, 120);
-  myGamePiece.gravity = 0.05;
-  myScore = new component("30px", "Consolas", "black", 280, 40, "text");
   container.start();
+  gameStarted = true;
 }
 
-function accelerateXY(n) {
-  myGamePiece.speedX = n;
-}
-
-function accelerateZ(n) {
-  myGamePiece.gravity = n;
-}
-
-function keyup(e) {
-  var code = e.keyCode;
-  if (metaPressed) {
+function keyup({ keyCode: code }) {
+  if (ctrlPressed) {
+    if (code === KEY.CTRL) {
+      ctrlPressed = false;
+    }
     return;
   }
-
-  switch (code) {
-    case KEY.Q:
-    case KEY.A:
-    case KEY.LEFT:
-    case KEY.D:
-    case KEY.RIGHT:
-      accelerateXY(0);
-      break;
-    case KEY.S:
-    case KEY.DOWN:
-    case KEY.Z:
-    case KEY.W:
-    case KEY.UP:
-    case KEY.SPACE:
-      accelerateZ(0.05);
-      break;
-    case KEY.R:
-      startGame();
-      break;
-    case KEY.CTRL:
-    case KEY.SHIFT:
-    case KEY.ALT:
-    case KEY.ESC:
-      metaPressed = false;
-      break;
+  if (gameStarted) {
+    if (KEY_UP.includes(code) || KEY_DOWN.includes(code)) {
+      player.move(undefined, 0);
+    } else if (KEY_LEFT.includes(code) || KEY_RIGHT.includes(code)) {
+      player.move(0);
+    }
   }
 }
 
-function keydown(e) {
-  var code = e.keyCode;
-  switch (code) {
-    case KEY.Q:
-    case KEY.A:
-    case KEY.LEFT:
-      accelerateXY(-4);
-      break;
-    case KEY.Z:
-    case KEY.W:
-    case KEY.UP:
-      accelerateZ(-0.2);
-      break;
-    case KEY.D:
-    case KEY.RIGHT:
-      accelerateXY(4);
-      break;
-    case KEY.S:
-    case KEY.DOWN:
-      accelerateZ(0.2);
-      break;
-    case KEY.SPACE:
-      accelerateZ(-0.2);
-      break;
-    case KEY.CTRL:
-    case KEY.SHIFT:
-    case KEY.ALT:
-    case KEY.ESC:
-      metaPressed = true;
-      break;
+function keydown({ keyCode: code }) {
+  if (code === KEY.R) {
+    startGame();
+  } else {
+    if (gameStarted) {
+      if (KEY_UP.includes(code)) {
+        player.move(undefined, -5);
+      } else if (KEY_DOWN.includes(code)) {
+        player.move(undefined, 5);
+      } else if (KEY_LEFT.includes(code)) {
+        player.move(-5);
+      } else if (KEY_RIGHT.includes(code)) {
+        player.move(5);
+      }
+    }
   }
 }
 
-document.addEventListener("DOMContentLoaded", function(event) {
+// 12, 20
+
+function generateWall(container) {
+  const height = 30;
+  const width = 30;
+  const w = new Set();
+  var x, y;
+  for (x = 0, y = 0; x < 20; x++) w.add({ x, y });
+  for (x = 0, y = 14; x < 20; x++) w.add({ x, y });
+  for (x = 0, y = 0; y < 15; y++) w.add({ x, y });
+  for (x = 19, y = 0; y < 15; y++) w.add({ x, y });
+
+  w.forEach(({ x, y }) => {
+    permanentWall.push(new WallComponent({ width, height, x: 0 + x * 30, y: 0 + y * 30, container }));
+  });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
   window.addEventListener('keydown', keydown, false);
   window.addEventListener('keyup', keyup, false);
   container = {
     canvas: document.getElementById("container"),
-    start: function() {
+    start: function () {
+      container = this;
       this.reset();
       this.context = this.canvas.getContext("2d");
       this.frameNo = 0;
-      this.canvas.width = 480;
-      this.canvas.height = 270;
+      this.canvas.width = MAX_WIDTH;
+      this.canvas.height = MAX_HEIGHT;
+      player = new Player({ container, color: 'red' });
+      score = new TextComponent({
+        size: '30px',
+        x: 10, y: MAX_HEIGHT - 10,
+        container,
+      });
+      generateWall(container);
+      permanentWall.push(new ObjectComponent({
+        width: 600,
+        height: 2,
+        x: 0,
+        y: MAX_HEIGHT - 42,
+        container,
+      }));
+
       this.interval = setInterval(updateGameArea, 20);
     },
-    clear: function() {
+    clear: function () {
       if (this.context) {
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
       }
     },
-    reset: function() {
+    reset: function () {
       this.clear();
       if (this.interval) {
         clearInterval(this.interval);
+        gameOver = false;
       }
-      myObstacles.length = 0;
+      permanentWall.length = 0;
     }
   }
 });

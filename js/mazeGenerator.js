040c144c018b98f32ede12d278132c33250a2893
fly <franck.ly247@gmail.com>
2017-09-12 10:20:34 -0400

    update maze

--- js/mazeGenerator.js
+++ js/mazeGenerator.js
@@ -1,15 +1,39 @@
+const ROOM_MIN_SIZE = 3;
+
 class MazeGenerator {
-  constructor(maxLeafSize, maxWidth, maxHeight) {
-    this.maxLeafSize = maxLeafSize;
-    this.maxWidth = maxHeight;
-    this.left = null;
-    this.right = null;
+  constructor(x, y, width, height) {
     this.random = new Random(42);
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.rooms = [];
+    this.walkable = [];
+    this.breakable = [];
+    this.walls = [];
   }
 
   split() {
-    for (var i = 10; i >= 0; i--) {
-      console.log(this.random.nextInt());
-    }
+    const split = false;
+
+    do {
+      var splitH = true;
+
+      if (width * 1.25 > height) {
+        splitH = false;
+      } else if (height * 1.25 > width) {
+        splitH = true;
+      } else if (this.random.nextRange(0, 2)) {
+        splitH = false;
+      }
+
+      if (splitH) {
+        const middle = this.random.nextRange(this.y + MIN_SIZE, this.height - MIN_SIZE);
+      } else {
+        const middle = this.random.nextRange(this.x + MIN_SIZE, this.width - MIN_SIZE);
+      }
+    } while (split);
   }
 }
+
+// 1,1 => 13,18
